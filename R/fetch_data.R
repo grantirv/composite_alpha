7c43db9ecd4e2cb0d38caa2ae46199a5f72556a8
Grant Irvine-Smith <grant.irvine-smith@ninetyone.com>
2025-10-26 20:19:14 +0000

    wip

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -6,8 +6,23 @@ require(data.table)
   analyst_scores <- DBI::dbGetQuery(cn, sql) |> as.data.table()
   fst::write_fst(analyst_scores, "data/analyst_scores.fst")
 
+# create recommendation data
+  recs <- analyst_scores[, .(
+    date = declaration_date,
+    dsseccode = sym::dsseccode(as.integer(dsinfocode)),
+    analyst = analyst_name,
+    rec = rec
+  )]
+  recs <- unique(recs)
+  fst::write_fst(recs, "data/recs.fst")
 
+# get esr & rtns
+  btd <- bt::read_btd("data/global-total.fst")
+  esr_rtn <- btd[, .(dsseccode, date, esr = fv, fwd_rtn)]
+  esr_rtn <- esr_rtn[date >= recs[, min(date)]]
 
+# combine, roll recs fwd by max 1q
+  comp_data <- recs[esr_rtn, on = .(dsseccode, date), roll = 92L]
+  comp_data[is.na(rec), rec := 0L]
+  fst::write_fst(comp_data, "data/comp_data.fst")
 
-
-  
--- R/model.R
+++ R/model.R
@@ -1,6 +1,17 @@
-d <- bt::read_btd("data/wolfe-total.fst")
+d <- fst::read_fst("data/comp_data.fst", as.data.table = TRUE)
 
-# add analyst recommendations
-  analyst_scores <- fst::read_fst("data/analyst_scores.fst", as.data.table = TRUE)
-  recs <- analyst_scores[, .(date = declaration_date, dsinfocode, rec)]
-  recs[, dsseccode ]
+# normalize, annualize and put on same units
+  d[, fwd_rtn := fwd_rtn - mean(fwd_rtn), by = date]
+  d[, fwd_rtn := (fwd_rtn + 1)^4 - 1]
+  d[, fwd_rtn := fwd_rtn * 100]
+  d[, esr := ((esr/100 + 1)^2 - 1) * 100]
+
+# split esr into pos and negative for kinked model
+  d[, esr_pos := pmax(0, esr)]
+
+m <- lm(fwd_rtn ~ esr + rec, data = d)
+
+
+# check coverage
+  cv <- d[!is.na(analyst), .(n_buys = sum(rec)), by = .(date, analyst)]
+  dcast(cv, date ~ analyst)
