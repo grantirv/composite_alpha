e226784d5c41474d0100aedff62977e119f22b3e
Grant Irvine-Smith <grant.irvine-smith@ninetyone.com>
2025-10-27 12:15:39 +0000

    wip

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -1,5 +1,7 @@
 require(data.table)
 
+MP <- "G:/Depts/EQUITY/4fquant/data/mosaic2.3/model_data/fr26/date-rgn-gics1/date-rgn-gics1/models"
+
 # fetch analyst data
   cn <- DBI::dbConnect(odbc::odbc(), "Databricks")
   sql <- "select * from f4.analysts.analyst_scores"
@@ -19,7 +21,22 @@ require(data.table)
   recs <- fst::read_fst("data/recs.fst", as.data.table = TRUE)
 
 # get esr & rtns
-  btd <- bt::read_btd("data/wolfe_investible-total.fst")
+  btd <- btd::f4_btd(
+    model_path = MP,
+    model = "Total",
+    universes_file = qu::f4quant("data/universes/universes.fst"),
+    universe = NULL,
+    start_date = min(recs$date),
+    end_date = max(recs$date),
+    freq = "half_year",
+    min_coverage = 100L,
+    return_outliers = c(-Inf, Inf),
+    ccy = "usd",
+    mcap_usdb = c(2, Inf),
+    liq_usdm = c(5, Inf)
+  )
+  bt::write_btd(btd, "data/btd.fst")
+  btd <- bt::read_btd("data/btd.fst")
   esr_rtn <- btd[, .(dsseccode, date, esr = fv, fwd_rtn)]
   esr_rtn <- esr_rtn[date >= recs[, min(date)]]
 
@@ -27,4 +44,3 @@ require(data.table)
   comp_data <- recs[esr_rtn, on = .(dsseccode, date), roll = 92L]
   comp_data[is.na(rec), rec := 0L]
   fst::write_fst(comp_data, "data/comp_data.fst")
-
